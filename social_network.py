af1d1759ce784f1e77f09f383b1070d00edc8a3e
nazrulhadi1 <nazrulhadi@abyres.net>
2023-03-23 12:07:59 +0800

    add the relationship for country

--- social_network.py
+++ social_network.py
@@ -6,27 +6,33 @@ clear_cyp ='''MATCH(n)
 DETACH DELETE n'''
 graph.run(clear_cyp)
 
+#load Country data
+country_cyp = '''load csv with  headers from "file:///dataset/sng_transaction.csv" as row
+WITH row WHERE row.passportnumber is not null
+MERGE (c:Country{country:row.country})'''
+
+graph.run(country_cyp)
+
 #load person data
 cyp = '''load csv with  headers from "file:///dataset/sng_trips.csv" as row
 WITH row WHERE row.passportnumber is not null
-MERGE (p:Person {name:row.name})-[:OWN]-(d:Passport {passport_number:row.passportnumber})-[:CITIZEN_OF]-(c:Country{country:row.citizenship})'''
+MERGE (p:Person {name:row.name, passport_number:row.passportnumber, country:row.citizenship})-[:OWN]-(d:Passport {passport_number:row.passportnumber})'''
 
 graph.run(cyp)
 
 #load company data
 comp_cyp ='''load csv with  headers from "file:///dataset/sng_work.csv" as row
 WITH row WHERE row.passportnumber is not null
-MERGE (p:Company {name_of_organization:row.nameoforganization, name:row.name})-[r:WORKING_AS]-(c:Position {designation:row.designation})-[:PERIOD_OF_SERVICES]-(d:Year{startyear:row.startyear, endyear:row.endyear})'''
+MERGE (p:Company {name_of_organization:row.nameoforganization, passport_number:row.passportnumber, country:row.country})-[r:WORKING_AS]-(c:Position {designation:row.designation})-[:PERIOD_OF_SERVICES]-(d:Year{startyear:row.startyear, endyear:row.endyear})'''
 
 graph.run(comp_cyp)
 
 #load education data
 edu_cyp = '''load csv with  headers from "file:///dataset/sng_education.csv" as row
 WITH row WHERE row.passportnumber is not null
-MERGE (p:Institution {name_of_institution:row.nameofinstitution, name:row.name})-[:Taking]-(c:Course{course: row.course})-[:STARTED_from]-(d:Study_Year{startyear:row.startyear, endyear:row.endyear})'''
+MERGE (p:Institution {name_of_institution:row.nameofinstitution, passport_number:row.passportnumber, country:row.country})-[:Taking]-(c:Course{course: row.course})-[:STARTED_from]-(d:Study_Year{startyear:row.startyear, endyear:row.endyear})'''
 
 graph.run(edu_cyp)
-
 
 #load credit card data
 cc_cyp = '''load csv with  headers from "file:///dataset/sng_transaction.csv" as row
@@ -54,28 +60,56 @@ graph.run(trip_cyp)
 
 #link person to company
 first_cyp = '''MATCH (p:Person),(c:Company)
-               WHERE p.name = c.name
-               REMOVE c.name
+               WHERE p.passport_number = c.passport_number
+               REMOVE c.passport_number
                MERGE (p)-[:WORKING_AT]-(c)'''
 graph.run(first_cyp)
 
-#link person to education
-second_cyp = '''MATCH (p:Person),(i:Institution)
-               WHERE p.name = i.name
-               REMOVE i.name
-               MERGE (p)-[:STUDY_AT]-(i)'''
+#link Person to country
+second_cyp = '''MATCH (p:Person), (ct:Country)
+               WHERE p.country = ct.country
+               REMOVE p.country
+               MERGE (p)-[:CITIZEN_OF]-(ct)'''
 graph.run(second_cyp)
 
+#link Company to country
+second_cyp = '''MATCH (c:Company), (ct:Country)
+               WHERE c.country = ct.country
+               REMOVE c.country
+               MERGE (c)-[:IN]-(ct)'''
+graph.run(second_cyp)
+
+#link person to education
+third_cyp = '''MATCH (i:Institution), (p:Person)
+               WHERE i.passport_number  = p.passport_number
+               REMOVE i.passport_number
+               MERGE (p)-[:STUDY_AT]-(i)'''
+graph.run(third_cyp)
+
+#link Education to Country
+fourth_cyp = '''MATCH (i:Institution),(ct:Country)
+               WHERE i.country  = ct.country 
+               REMOVE i.country
+               MERGE (i)-[:IN]-(ct)'''
+graph.run(fourth_cyp)
 
 #link person to credit card
-third_cyp = '''MATCH (p:Person),(c:Credit_Card)
+fifth_cyp = '''MATCH (p:Person),(c:Credit_Card)
                WHERE p.cardnumber = c.cardnumber
                REMOVE p.cardnumber
                MERGE (p)-[:OWN]-(c)'''
-graph.run(third_cyp)
+graph.run(fifth_cyp)
 
-#link person to trip
-fourth_cyp = '''MATCH (p:Person),(pp:Passport),(t:Trip)
-               WHERE pp.passport_number = t.passport_number
-               MERGE (p)-[:WENT]-(t)'''
-graph.run(fourth_cyp)
+#link Person to Trip
+six_cyp = '''MATCH (p:Person),(c:Trip)
+               WHERE p.passport_number = c.passport_number
+               REMOVE p.passport_number
+               MERGE (p)-[:WENT_TO]-(c)'''
+graph.run(six_cyp)
+
+#link Trip to Country
+seven_cyp = '''MATCH (p:Trip),(c:Country)
+               WHERE p.departure_from = c.country
+               REMOVE p.departure_from
+               MERGE (p)-[:DEPATURE_FROM]-(c)'''
+graph.run(seven_cyp)
